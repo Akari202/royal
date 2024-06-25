d71a88cb01531492642b06e078afe4635716d679
akari <akaritwo0two@gmail.com>
2024-06-25 11:37:09 -0600

    switch

--- src/gcode.rs
+++ src/gcode.rs
@@ -14,8 +14,8 @@ pub fn test() {
 fn test_lexer() {
     // load NC/test.NC
     // let input = include_str!("../NC/rear_upright.NC");
-    // let input = include_str!("../NC/test_arc.NC");
-    let input = include_str!("../NC/brakes.gcode");
+    let input = include_str!("../NC/test_arc.NC");
+    // let input = include_str!("../NC/brakes.gcode");
     // semicolon("./NC/brakes.gcode".into()).unwrap();
     // let input = include_str!("../NC/Purple Worm Fewer supports+gcode - 4760012/files/PW_pt1.gcode");
     let mut program = points::Program::from_file(input).unwrap();
